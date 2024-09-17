8fa1d7054185f71e8c6c63318383563838eea83f
Jacob <jacobo.jimenez@n5now.com>
2024-09-16 19:16:49 -0500

    add security, helmet

--- app/api/initialTasks/route.js
+++ app/api/initialTasks/route.js
@@ -1,13 +1,22 @@
 // app/api/initialTasks/route.js
-import { NextResponse } from 'next/server';
-import { getInitialTasks } from '../../utils/getInitialTasks'; // Ruta relativa
+import { NextResponse } from "next/server";
+import { getInitialTasks } from "../../utils/getInitialTasks"; // Ruta relativa
 
 export async function GET() {
   try {
-    const tasks = await getInitialTasks(); 
-    return NextResponse.json(tasks); 
+    const tasks = await getInitialTasks();
+
+    // Validación básica de datos (ejemplo)
+    if (!Array.isArray(tasks)) {
+      throw new Error("Invalid data format");
+    }
+
+    return NextResponse.json(tasks);
   } catch (error) {
-    console.error('Error al obtener las tareas iniciales:', error);
-    return NextResponse.error(); 
+    console.error("Error al obtener las tareas iniciales:", error);
+    return NextResponse.json(
+      { error: "Error al obtener las tareas" },
+      { status: 500 }
+    );
   }
 }
