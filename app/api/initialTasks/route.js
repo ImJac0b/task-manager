14f7eefa1668cd31a2bed0b83de5eb07d493eefc
Jacob <jacobo.jimenez@n5now.com>
2024-09-16 19:17:43 -0500

    delete comments

--- app/api/initialTasks/route.js
+++ app/api/initialTasks/route.js
@@ -1,12 +1,9 @@
-// app/api/initialTasks/route.js
 import { NextResponse } from "next/server";
-import { getInitialTasks } from "../../utils/getInitialTasks"; // Ruta relativa
+import { getInitialTasks } from "../../utils/getInitialTasks";
 
 export async function GET() {
   try {
     const tasks = await getInitialTasks();
-
-    // Validación básica de datos (ejemplo)
     if (!Array.isArray(tasks)) {
       throw new Error("Invalid data format");
     }
